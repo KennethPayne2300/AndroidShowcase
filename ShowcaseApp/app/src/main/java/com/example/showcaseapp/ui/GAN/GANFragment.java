01b71e6d6d219a7d5518c90e495f51b02d721026
KennethPayne2300 <kennethpayne353@gmail.com>
2025-11-25 16:09:23 -0600

    started coding GAN

--- ShowcaseApp/app/src/main/java/com/example/showcaseapp/ui/GAN/GANFragment.java
+++ ShowcaseApp/app/src/main/java/com/example/showcaseapp/ui/GAN/GANFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,8 +25,38 @@ public class GANFragment extends Fragment {
         binding = FragmentGanBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGan;
-        GanViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        int attempts=10;
+        int secretNumber = (int) (Math.random()*100)+1;
+
+        binding.GuessBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView guess = binding.GuessTXT;
+
+                try{
+                    int numberGuess = Integer.parseInt(guess.getText().toString());
+
+                    if(numberGuess<=100 || numberGuess>0){
+                        if(numberGuess==secretNumber){
+                            attempts=10;
+                            binding.FeedbackTXT.setText("You Got It!!!");
+                        }else if(numberGuess>secretNumber){
+                            attempts--;
+                            binding.FeedbackTXT.setText("Too High");
+                        }else{
+                            attempts--;
+                            binding.FeedbackTXT.setText("Too Low");
+                        }
+                        binding.attemptsTXT.setText(attempts);
+                    }else{
+                        Toast.makeText(getContext(),"needs to be between 1 and 100",Toast.LENGTH_SHORT).show();
+                    }
+                } catch (NumberFormatException e){
+                    Toast.makeText(getContext(), "Need a number, and can't be a decimal either", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         return root;
     }
 
@@ -33,5 +64,9 @@ public class GANFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void addNamesToLeaderBoard(String name, int score){
+
     }
 }
