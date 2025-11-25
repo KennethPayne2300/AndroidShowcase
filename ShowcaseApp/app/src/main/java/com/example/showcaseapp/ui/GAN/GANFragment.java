77b1de4d1a1271fd49d70f680b71af8bdb3b4d1d
ImperialOfficer <payne.kenneth.54321@gmail.com>
2025-11-25 14:03:24 -0600

    finished GAN functionality

--- ShowcaseApp/app/src/main/java/com/example/showcaseapp/ui/GAN/GANFragment.java
+++ ShowcaseApp/app/src/main/java/com/example/showcaseapp/ui/GAN/GANFragment.java
@@ -12,12 +12,14 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.showcaseapp.R;
 import com.example.showcaseapp.databinding.FragmentGanBinding;
 
 public class GANFragment extends Fragment {
 
     public int attempts=10;
     public int points=0;
+    public int level=1;
 
     public int secretNumber = (int) (Math.random()*100)+1;
 
@@ -32,42 +34,50 @@ public class GANFragment extends Fragment {
         View root = binding.getRoot();
 
         binding.pointsTXT.setText("Points: 0");
+        binding.levelTXT.setText("Level: 1");
 
         binding.GuessBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TextView guess = binding.GuessTXT;
 
-                if(attempts<=0){
-                    try{
-                        int numberGuess = Integer.parseInt(guess.getText().toString());
+                try{
+                    int numberGuess = Integer.parseInt(guess.getText().toString());
 
-                        if(numberGuess<=100 && numberGuess>0){
-                            if(numberGuess==secretNumber){
-                                secretNumber = (int) (Math.random()*100)+1;
-                                points+=(50+(5*(10-attempts)));
-                                Log.d("points: ", attempts+"");
-                                binding.pointsTXT.setText(String.format("Points: %d", points));
-                                binding.FeedbackTXT.setText("You Got It!!!");
-                                attempts=10;
-                            }else if(numberGuess>secretNumber){
-                                attempts--;
-                                binding.FeedbackTXT.setText("Too High");
-                            }else{
-                                attempts--;
-                                binding.FeedbackTXT.setText("Too Low");
-                            }
-                            binding.attemptsTXT.setText(String.format("%d", attempts));
-                            binding.GuessTXT.setText("");
+                    if(numberGuess<=100 && numberGuess>0){
+                        if(numberGuess==secretNumber){
+                            points+=(50+(5*attempts));
+                            level++;
+                            binding.levelTXT.setText(String.format("Level: %d",level));
+                            binding.pointsTXT.setText(String.format("Points: %d", points));
+                            binding.FeedbackTXT.setText(String.format("You Got It!!! the number was: %d",secretNumber));
+                            secretNumber = (int) (Math.random()*100)+1;
+                            attempts=10;
+                        }else if(numberGuess>secretNumber){
+                            attempts--;
+                            binding.FeedbackTXT.setText("Too High");
                         }else{
-                            Toast.makeText(getContext(),"needs to be between 1 and 100",Toast.LENGTH_SHORT).show();
+                            attempts--;
+                            binding.FeedbackTXT.setText("Too Low");
                         }
-                    } catch (NumberFormatException e){
-                        Toast.makeText(getContext(), "Need a number, and can't be a decimal either", Toast.LENGTH_SHORT).show();
+                        binding.attemptsTXT.setText(String.format("%d", attempts));
+                        binding.GuessTXT.setText("");
+                        if(attempts<=0||level>5){
+                            binding.GameOverLayout.setVisibility(View.VISIBLE);
+                            binding.GameLayout.setVisibility(View.GONE);
+                            if(level>5){
+                                binding.GameOverTXT.setText("Game Over you Won!!!");
+                                binding.gameOverImg.setImageResource(R.drawable.gan_win);
+                            } else {
+                                binding.GameOverTXT.setText("Game Over you Lost");
+                                binding.gameOverImg.setImageResource(R.drawable.gan_lose);
+                            }
+                        }
+                    }else{
+                        Toast.makeText(getContext(),"needs to be between 1 and 100",Toast.LENGTH_SHORT).show();
                     }
-                } else {
-                    binding.LeadeerBoard.setVisibility(View.VISIBLE);
-                    binding.GameLayout.setVisibility(View.GONE);
+                } catch (NumberFormatException e){
+                    Toast.makeText(getContext(), "Need a round number", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -77,10 +87,14 @@ public class GANFragment extends Fragment {
             public void onClick(View v) {
                 attempts=10;
                 points=0;
+                level=1;
+                secretNumber = (int) (Math.random()*100)+1;
                 binding.FeedbackTXT.setText("");
                 binding.GuessTXT.setText("");
+                binding.attemptsTXT.setText("10");
                 binding.pointsTXT.setText("Points: 0");
-                binding.LeadeerBoard.setVisibility(View.GONE);
+                binding.levelTXT.setText("Level: 1");
+                binding.GameOverLayout.setVisibility(View.GONE);
                 binding.GameLayout.setVisibility(View.VISIBLE);
             }
         });
@@ -92,9 +106,5 @@ public class GANFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }
-
-    public void addNamesToLeaderBoard(String name, int score){
-
     }
 }
